db72047a4cff9fceb905b5fe8b0b681083239255
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-20 22:56:32 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,25 @@
+startButton = document.querySelector('button[data-start]');
+stopButton = document.querySelector('button[data-stop]');
+body = document.querySelector('body');
+console.log(body)
+stopButton.disabled = true;
+startButton.addEventListener('click', changeColor);
+stopButton.addEventListener('click', stopChangeColor);
 
-console.log('hello')
+function changeColor() {
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+       startButton.disabled = true;
+  stopButton.disabled = false;
+};
+
+function stopChangeColor() {
+    clearInterval(interval);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
