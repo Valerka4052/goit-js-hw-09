083f92308cc2b0b6302da58f563d0af33aa8d1af
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-21 15:14:31 +0000

    Deploying to gh-pages from @ Valerka4052/goit-js-hw-09@f045b67278d7316c9b5102c3be94457b14ce2ed6 🚀

--- 01-color-switcher.bf9bbd6d.js
+++
@@ -1,2 +0,0 @@
-body=document.querySelector("body"),console.log(body);
-//# sourceMappingURL=01-color-switcher.bf9bbd6d.js.map
--- 01-color-switcher.d637bccd.js
+++
@@ -1,2 +0,0 @@
-body=document.querySelector("body"),console.log(body);
-//# sourceMappingURL=01-color-switcher.d637bccd.js.map
