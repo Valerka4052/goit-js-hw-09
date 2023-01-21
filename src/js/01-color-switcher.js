9142188f14d7f8707601765ef888eff9d0e9b55f
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-21 20:48:38 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,26 @@
-startButton = document.querySelector('button[data-start]');
-stopButton = document.querySelector('button[data-stop]');
-body = document.querySelector('body');
-console.log(body)
-stopButton.disabled = true;
-startButton.addEventListener('click', changeColor);
-stopButton.addEventListener('click', stopChangeColor);
+// startButton = document.querySelector('button[data-start]');
+// stopButton = document.querySelector('button[data-stop]');
+// body = document.querySelector('body');
+// console.log(body)
+// stopButton.disabled = true;
+// startButton.addEventListener('click', changeColor);
+// stopButton.addEventListener('click', stopChangeColor);
 
-function changeColor() {
-    interval = setInterval(() => {
-        body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
-       startButton.disabled = true;
-  stopButton.disabled = false;
-};
+// function changeColor() {
+//     interval = setInterval(() => {
+//         body.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//        startButton.disabled = true;
+//   stopButton.disabled = false;
+// };
 
-function stopChangeColor() {
-    clearInterval(interval);
-    startButton.disabled = false;
-    stopButton.disabled = true;
-};
+// function stopChangeColor() {
+//     clearInterval(interval);
+//     startButton.disabled = false;
+//     stopButton.disabled = true;
+// };
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// };
+console.log('привет')
