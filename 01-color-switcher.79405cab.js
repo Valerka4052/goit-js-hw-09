c704121edc78fb5982a38c612108320cfedf8132
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-21 18:49:09 +0000

    Deploying to gh-pages from @ Valerka4052/goit-js-hw-09@9142188f14d7f8707601765ef888eff9d0e9b55f 🚀

---
+++ 01-color-switcher.79405cab.js
@@ -0,0 +1,2 @@
+console.log("привет");
+//# sourceMappingURL=01-color-switcher.79405cab.js.map
--- 01-color-switcher.84f2508e.js
+++
@@ -1,2 +0,0 @@
-startButton=document.querySelector("button[data-start]"),stopButton=document.querySelector("button[data-stop]"),body=document.querySelector("body"),console.log(body),stopButton.disabled=!0,startButton.addEventListener("click",(function(){interval=setInterval((()=>{body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),startButton.disabled=!0,stopButton.disabled=!1})),stopButton.addEventListener("click",(function(){clearInterval(interval),startButton.disabled=!1,stopButton.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.84f2508e.js.map
